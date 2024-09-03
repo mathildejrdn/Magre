ae5462f819a14ec6670177d8d525c8b45992764b
GregoryVDev <arexo@hotmail.fr>
2024-09-03 09:25:38 +0200

    Added button and icons media social

--- src/inscription.php
+++ src/inscription.php
@@ -15,31 +15,35 @@
         <form method="POST">
             <img src="" alt="">
             <h1>Inscription</h1>
-            <div class="container-name">
-                <label for="name">Nom :</label>
+            <div class="container-surname">
+                <label for="surname">Nom :</label>
                 <input type="text" class="form.input" name="surname" id="surname" placeholder="Nom de famille">
             </div>
             <div class="container-name">
                 <label for="name">Prénom :</label>
-                <input type="text" class="form.input" name="name" id="name">
+                <input type="text" class="form.input" name="name" id="name" placeholder="Prénom">
             </div>
-            <div class="container-name">
-                <label for="name">Email :</label>
-                <input type="text" class="form.input" name="name" id="name">
+            <div class="container-email">
+                <label for="email">Email :</label>
+                <input type="email" class="form.input" name="email" id="email" placeholder="Email">
             </div>
-            <div class="container-name">
-                <label for="name">Mot de passe :</label>
-                <input type="text" class="form.input" name="name" id="name">
+            <div class="container-password">
+                <label for="pass">Mot de passe :</label>
+                <input type="password" class="form.input" name="pass" id="pass" placeholder="Mot de passe">
             </div>
-            <div class="container-name">
-                <label for="name">Confirmation :</label>
-                <input type="text" class="form.input" name="name" id="name">
+            <div class="container-confirm">
+                <label for="pass2">Confirmation :</label>
+                <input type="password" class="form.input" name="pass2" id="pass2" placeholder="Confirmation de mot de passe">
             </div>
-
+            <button type="submit" class="connexion-button">S'inscrire</button>
+            <p>Vous avez déjà un compte ? <a href="connexion.php">Connectez-vous</a></p>
+            <span class="follow">Suivez-nous</span>
+            <div class="container-logos">
+                <a href="#"><img src="./img/connexion/facebook.svg" alt="Logo facebook"></a>
+                <a href="#"><img src="./img/connexion/instagram.svg" alt="Logo instagram"></a>
+                <a href="#"><img src="./img/connexion/twitter-x.svg" alt="Logo twitter"></a>
+            </div>
         </form>
-
-
-
     </main>
 
 </body>
