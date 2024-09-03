7783bf9004033504264e57bbbd14df7af65cd0fc
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-09-03 09:21:55 +0200

    index du back office

---
+++ src/backoffice/backindex.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Images côte à côte avec texte</title>
+    <link rel="stylesheet" href="../styles/backoffice.css">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="image-container">
+            <img src="../images/backoffice/gestion.jpg" alt="image d'illustration gestion des propriétés">
+            <div class="overlay-text">Gestion des propriétés</div>
+        </div>
+        <div class="image-container">
+            <img src="../images/backoffice/calendrier.jpg" alt="image d'illustration calendrier">
+            <div class="overlay-text">Calendrier de rendez-vous</div>
+        </div>
+        <div class="image-container">
+            <img src="../images/backoffice/vendu.jpg" alt="image d'illustration historique des ventes">
+            <div class="overlay-text">Historique des ventes</div>
+        </div>
+    </div>
+</body>
+</html>
