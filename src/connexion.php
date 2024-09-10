e43cdf4abbe4ba6a0aa31809cfccef15b885bcad
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-09-10 14:48:37 +0200

    connexion php

--- src/connexion.php
+++ src/connexion.php
@@ -1,3 +1,74 @@
+<?php
+// Vérifie que c'est une adresse email conforme pour sécuriser
+function ValidateEmail($email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+}
+
+// Ici on vérifie que la variable $_POST n'est pas vide
+if (!empty($_POST)) {
+    if (
+        isset($_POST["email"], $_POST["pass"]) &&
+        !empty($_POST["email"]) &&
+        !empty($_POST["pass"]) 
+    ) {
+        // Vérifie aussi que l'user a mis une vraie adresse email en appelant la fonction établie plus haut
+        if (!ValidateEmail($_POST["email"])) {
+            die("Ce n'est pas une adresse email valide.");
+        }
+
+        //connexion BDD
+        require_once('./connect.php');
+
+        //requête SQL
+        $sql = "SELECT * FROM 'user' WHERE 'email' = :email";
+        
+        //préparation requête
+        $query = $db -> prepare ($sql);
+    
+        $query->bindValue(':email', $_POST['email']);
+
+        //exécution de la requête
+        $query->execute() 
+
+        $user = $query->fetch();
+
+        if (!$user) {
+            die("l'utilisateur ou/et le mot de passe est invalide");
+        }
+
+        //si l'user existe on peut vérifier son mdp
+        if (!password_verify)($_POST["pass"], $user["pass"])) 
+        {
+            die("l'utilisateur ou/et le mot de passe est invalide");            
+        }
+
+        //utilisateur et MDP corrects, on continue pour se connecter en stockat dans la session les infos utilisateur
+        $_SESSION ["user"] = [
+            "user_id" => $user ["id"],
+            "name" => $name ["name"],
+            "surname" => $surname ["surname"],
+            "email" => $email ["email"],
+            "role" => $role ["role"] 
+        ] ;
+
+        //reconnaissance du rôle de l'utilisateur après la connexion
+        if(!$user ["role"] == "admin") {
+            $redirectUrl = "../backindex.php";
+        } elseif { 
+            (!$user ["role"] == "user")
+            $redirectUrl= "./index.php" ;
+        } else {
+            die("aucun rôle n'a été trouvé");
+        }
+
+        header(location: $redirectUrl);
+        exit ();
+        else {
+            die('formulaire incomplet');
+        }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
