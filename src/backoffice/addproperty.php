c9ccf5964279c5a381d4642fb7cdc5f54977726d
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-09-12 14:06:00 +0200

    formulaire d'ajout d'une propriété

--- src/backoffice/addproperty.php
+++ src/backoffice/addproperty.php
@@ -1,1 +1,82 @@
-ajouter maison
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index de gestion</title>
+    <link rel="stylesheet" href="../styles/backoffice.css">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <script src="../javascript/script.js" defer></script>
+</head>
+<body>
+<div class="container">
+    <div class="form-container">
+        <img src="../images/logos/MG logo trans.png" alt="Logo">
+        <form method="POST">
+            <div class="container-price">
+                <label for="price">Prix :</label>
+                <input type="number" class="form-input" name="price" id="price" placeholder="prix en valeur numérique">
+            </div>
+            <div class="container-region">
+                <label for="region">Région :</label>
+                <select class="form-input" name="region" id="region">
+                    <option value="">Sélectionnez une région</option>
+                    <option value="Auvergne-Rhône-Alpes">Auvergne-Rhône-Alpes</option>
+                    <option value="Bourgogne-Franche-Comté">Bourgogne-Franche-Comté</option>
+                    <option value="Bretagne">Bretagne</option>
+                    <option value="Centre-Val de Loire">Centre-Val de Loire</option>
+                    <option value="Corse">Corse</option>
+                    <option value="Grand Est">Grand Est</option>
+                    <option value="Hauts-de-France">Hauts-de-France</option>
+                    <option value="Île-de-France">Île-de-France</option>
+                    <option value="Normandie">Normandie</option>
+                    <option value="Nouvelle-Aquitaine">Nouvelle-Aquitaine</option>
+                    <option value="Occitanie">Occitanie</option>
+                    <option value="Pays de la Loire">Pays de la Loire</option>
+                    <option value="Provence-Alpes-Côte d'Azur">Provence-Alpes-Côte d'Azur</option>
+                </select>
+            </div>
+            <select class="form-input" name="ID_category" id="category">
+                <option value="">Sélectionnez une catégorie</option>
+                <option value="Châlet">Châlet</option>
+                <option value="Château">Château</option>
+                <option value="Villa">Villa</option>
+                <option value="Appartement">Appartement</option>
+            </select>
+            <div class="container-bedrooms">
+                <label for="bedrooms">Nombre de chambres :</label>
+                <input type="number" class="form-input" name="bedroom" id="bedroom" placeholder="nombre de chambre">
+            </div>
+            <div class="container-rooms">
+                <label for="rooms">Nombre de pièces :</label>
+                <input type="number" class="form-input" name="room" id="room" placeholder="nombre de pièces">
+            </div>
+            <div class="container-square_meters">
+                <label for="square_meters">M² :</label>
+                <input type="number" class="form-input" name="square_meters" id="square_meters" placeholder="m² en valeur numérique">
+            </div>
+            <div class="container-square_meters">
+                <label for="description">Description du bien :</label>
+                <textarea class="form-input" name="description" id="description" placeholder="Description du bien" rows="5" cols="40"></textarea>
+            </div>
+            <select class="form-input" name="type" id="type">
+                <option value="">Est-ce une vente ou une location ?</option>
+                <option value="vente">Vente</option>
+                <option value="location">Location</option>
+            </select>
+            <div class="container-postdate">
+                <label for="postdate">Date de publication :</label>
+                <input type="date" class="form-input" name="postdate" id="postdate">
+            </div>
+            <div class="container-image">
+                <label for="image">Ajouter une image :</label>
+                <input type="file" class="form-input" name="image" id="image" accept="image/*">
+            </div>
+            <button type="submit">Ajouter une propriété</button>
+        </form>
+    </div>
+</div>
+
+
+</body>
+</html>
--- src/backoffice/form_infos.php
+++ src/backoffice/form_infos.php
@@ -1,1 +1,48 @@
-formulaire infos
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index de gestion</title>
+    <link rel="stylesheet" href="../styles/backoffice.css">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/index.css">
+    <script src="../javascript/script.js" defer></script>
+</head>
+<body>
+<form method="POST">
+            <img src="../images/logos/MG logo trans.png" alt="Logo">
+            <div class="container-surname">
+                <label for="surname">Nom :</label>
+                <input type="text" class="form.input" name="surname" id="surname" placeholder="Nom">
+            </div>
+            <div class="container-name">
+                <label for="name">Prénom :</label>
+                <input type="text" class="form.input" name="name" id="name" placeholder="Prénom">
+            </div>
+            <div class="container-email">
+                <label for="email">Email :</label>
+                <input type="email" class="form.input" name="email" id="email" placeholder="Email">
+            </div>
+            <div class="container-password">
+                <label for="pass">Mot de passe :</label>
+                <input type="password" class="form.input" name="pass" id="pass" placeholder="Mot de passe">
+            </div>
+            <div class="container-confirm">
+                <label for="pass2">Confirmation :</label>
+                <input type="password" class="form.input" name="pass2" id="pass2" placeholder="Confirmation">
+            </div>
+            <button type="submit" class="connexion-button">S'inscrire</button>
+
+            <div class="container-login">
+                <p>Avez-vous un compte ?</p>
+                <a href="connexion.php">Se connecter</a>
+            </div>
+
+
+        </form>
+
+    <button><a href="./addproperty.php">Ajouter une propriété</a></button>
+
+</body>
+</html>
