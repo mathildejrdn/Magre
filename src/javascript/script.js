6fb5a4f691b9dd5e26e4a6eedb52e19e382880db
GregoryVDev <arexo@hotmail.fr>
2024-09-13 15:09:24 +0200

    Added desktop navbar and burger menu and added images

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,82 +1,98 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Fonction pour afficher/masquer le filtre au clic sur l'en-tête
-    document.querySelectorAll('th').forEach(function(th) {
-        th.addEventListener('click', function(event) {
-            var filterContainer = th.querySelector('.filter-container');
+// MENU BURGER
 
-            // Ferme tous les autres filtres
-            document.querySelectorAll('.filter-container').forEach(function(container) {
-                if (container !== filterContainer) {
-                    container.classList.remove('active');
-                }
-            });
+const nav = document.getElementById("nav");
+const menuBurger = document.getElementById("burger");
 
-            // Active/désactive le filtre actuel
-            if (filterContainer) {
-                filterContainer.classList.toggle('active');
-            }
+menuBurger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  menuBurger.classList.toggle("active");
+});
 
-            event.stopPropagation(); // Empêche la fermeture immédiate du filtre
+document.addEventListener("DOMContentLoaded", function () {
+  // Fonction pour afficher/masquer le filtre au clic sur l'en-tête
+  document.querySelectorAll("th").forEach(function (th) {
+    th.addEventListener("click", function (event) {
+      var filterContainer = th.querySelector(".filter-container");
+
+      // Ferme tous les autres filtres
+      document
+        .querySelectorAll(".filter-container")
+        .forEach(function (container) {
+          if (container !== filterContainer) {
+            container.classList.remove("active");
+          }
         });
-    });
 
-    // Empêche la fermeture du filtre lors de l'interaction avec le contenu du filtre
-    document.querySelectorAll('.filter-container').forEach(function(container) {
-        container.addEventListener('click', function(event) {
-            event.stopPropagation();
-        });
-    });
+      // Active/désactive le filtre actuel
+      if (filterContainer) {
+        filterContainer.classList.toggle("active");
+      }
 
-    // Ferme les filtres si on clique en dehors
-    document.addEventListener('click', function() {
-        document.querySelectorAll('.filter-container').forEach(function(container) {
-            container.classList.remove('active');
-        });
+      event.stopPropagation(); // Empêche la fermeture immédiate du filtre
     });
+  });
 
-    // Fonction pour formater les prix
-    function formatPrice(price) {
-        let value = parseFloat(price.replace(/[^0-9]/g, ''));
-        if (value >= 1000000) {
-            return (value / 1000000).toFixed(0) + ' million(s) d\'€'; // Pas de décimales
-        } else if (value >= 1000) {
-            return (value / 1000).toFixed(0) + ' mille €'; // Pas de décimales
-        } else {
-            return value + ' €'; // Affiche le prix tel quel
-        }
+  // Empêche la fermeture du filtre lors de l'interaction avec le contenu du filtre
+  document.querySelectorAll(".filter-container").forEach(function (container) {
+    container.addEventListener("click", function (event) {
+      event.stopPropagation();
+    });
+  });
+
+  // Ferme les filtres si on clique en dehors
+  document.addEventListener("click", function () {
+    document
+      .querySelectorAll(".filter-container")
+      .forEach(function (container) {
+        container.classList.remove("active");
+      });
+  });
+
+  // Fonction pour formater les prix
+  function formatPrice(price) {
+    let value = parseFloat(price.replace(/[^0-9]/g, ""));
+    if (value >= 1000000) {
+      return (value / 1000000).toFixed(0) + " million(s) d'€"; // Pas de décimales
+    } else if (value >= 1000) {
+      return (value / 1000).toFixed(0) + " mille €"; // Pas de décimales
+    } else {
+      return value + " €"; // Affiche le prix tel quel
     }
+  }
 
-    // Fonction pour appliquer les filtres
-    function applyFilters() {
-        console.log("Applying filters..."); // Debugging
+  // Fonction pour appliquer les filtres
+  function applyFilters() {
+    console.log("Applying filters..."); // Debugging
 
-        var selectedRegion = document.getElementById('region-select').value;
+    var selectedRegion = document.getElementById("region-select").value;
 
-        console.log("Selected Region:", selectedRegion); // Debugging
+    console.log("Selected Region:", selectedRegion); // Debugging
 
-        // Parcourt toutes les lignes du tableau et applique les filtres
-        document.querySelectorAll('tbody tr').forEach(function(row) {
-            var rowRegion = row.cells[2].textContent.trim(); // Notez que les index des cellules peuvent changer
+    // Parcourt toutes les lignes du tableau et applique les filtres
+    document.querySelectorAll("tbody tr").forEach(function (row) {
+      var rowRegion = row.cells[2].textContent.trim(); // Notez que les index des cellules peuvent changer
 
-            // Détermine si la ligne correspond aux critères de filtre
-            var regionMatch = selectedRegion === "" || rowRegion === selectedRegion;
+      // Détermine si la ligne correspond aux critères de filtre
+      var regionMatch = selectedRegion === "" || rowRegion === selectedRegion;
 
-            // Affiche la ligne si elle correspond aux critères, sinon la cache
-            if (regionMatch) {
-                row.style.display = '';
-            } else {
-                row.style.display = 'none';
-            }
-        });
-    }
-
-    // Met à jour les prix au format désiré
-    document.querySelectorAll('tbody tr').forEach(function(row) {
-        var priceCell = row.cells[1]; // Assurez-vous que l'index correspond à la colonne du prix
-        var formattedPrice = formatPrice(priceCell.textContent);
-        priceCell.textContent = formattedPrice;
+      // Affiche la ligne si elle correspond aux critères, sinon la cache
+      if (regionMatch) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
     });
+  }
 
-    // Ajouter les événements pour les filtres régionaux
-    document.getElementById('apply-region-filter').addEventListener('click', applyFilters);
+  // Met à jour les prix au format désiré
+  document.querySelectorAll("tbody tr").forEach(function (row) {
+    var priceCell = row.cells[1]; // Assurez-vous que l'index correspond à la colonne du prix
+    var formattedPrice = formatPrice(priceCell.textContent);
+    priceCell.textContent = formattedPrice;
+  });
+
+  // Ajouter les événements pour les filtres régionaux
+  document
+    .getElementById("apply-region-filter")
+    .addEventListener("click", applyFilters);
 });
--- src/template/header.php
+++ src/template/header.php
@@ -5,15 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/header.css">
+    <link rel="stylesheet" href="../styles/reset.css">
     <title>Magré's</title>
 </head>
 
 <body>
-    <header>
-        <div class="top-nav">
-
+    <div class="navbar-top">
+        <div class="container-icons">
+            <a href="#"><img src="../images/logos/dashboard.svg" alt="Dashboard"></a>
+            <div class="dr"></div>
+            <a href="#"><img src="../images/logos/user.svg" alt="Connexion"> </a>
+            <div class="dr"></div>
+            <a href="#"><img src="../images/logos/heart.svg" alt="Favoris"></a>
+            <div class="dr"></div>
+            <a href="#"><img src="../images/logos/flag-UK.svg" alt="Drapeau anglais"></a>
         </div>
+        <div class="container-supports">
+            <a href="#"><img src="../images/logos/phone.svg" alt="Téléphone">(+33)1.02.03.04.05</a>
+            <div class="dr"></div>
+            <a href="#"><img src="../images/logos/mail.svg" alt="Contact">contact@magre.fr</a>
+        </div>
+    </div>
+    <nav>
+        <img src="../images/logos/logo.png" class="logo" alt="Logo">
+        <div id="nav">
+            <ul>
+                <li><a href="#">Biens</a></li>
+                <div class="dr"></div>
+                <li><a href="#">Régions</a></li>
+                <div class="dr"></div>
+                <li><a href="#">Services</a></li>
+                <div class="dr"></div>
+                <li><a href="#">Agence</a></li>
+            </ul>
+            <div id="burger">
+                <img src="../images/logos/menu.svg" alt="Menu Burger">
+            </div>
+        </div>
+    </nav>
     </header>
 </body>
+<script src="../javascript/script.js"></script>
 
 </html>
